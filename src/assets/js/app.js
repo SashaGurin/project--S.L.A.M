50fcc2f74cd66b36c3774652adf505d1c1ebef72
SashaGurin <top-on3@mai.ru>
2022-02-16 17:02:26 +0300

    fix_slider-section__baner

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,21 +1,23 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   // Optional parameters
   // direction: 'vertical',
   loop: false,
-
+  autoplay: {
+    delay: 5000,
+  },
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 
-  // And if we need scrollbar
+
   // scrollbar: {
   //   el: '.swiper-scrollbar',
   // },
