251b9c8f05d017f4d2ad2fa1a92669e68bfaf0f4
SashaGurin <top-on3@mai.ru>
2022-02-22 16:01:11 +0300

    fix form-style

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -1,53 +1,46 @@
 const swiper = new Swiper(".image-slider", {
-  // Optional parameters
-  // direction: 'vertical',
   loop: true,
   autoplay: {
     delay: 5000,
   },
 
-  // If we need pagination
   pagination: {
     el: ".swiper-pagination",
-    type: 'bullets',
-    // clickable:true,
+    type: "bullets",
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   preloadImages: false,
-  // Enable lazy loading
   lazy: true,
-  // loop:true,
-
 });
 
-document.getElementsByClassName("image-slider")[0].addEventListener("mouseover", function( ) {
-  swiper.autoplay.stop();
-});
+document
+  .getElementsByClassName("image-slider")[0]
+  .addEventListener("mouseover", function () {
+    swiper.autoplay.stop();
+  });
 
-document.getElementsByClassName("image-slider")[0].addEventListener("mouseout", function( ) {
-  swiper.autoplay.start();
-});
+document
+  .getElementsByClassName("image-slider")[0]
+  .addEventListener("mouseout", function () {
+    swiper.autoplay.start();
+  });
 
 const myswiper2 = new Swiper(".promo-slider", {
-  // Optional parameters
   loop: true,
   slidesPerView: 4,
   slidesPerGroup: 10,
   spaceBetween: 20,
 
-  // If we need pagination
   pagination: {
     el: ".swiper-pagination",
-    type: 'bullets',
-    clickable: true
+    type: "bullets",
+    clickable: true,
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next.promo--slider.promo_arrow-next",
     prevEl: " .swiper-button-prev.promo--slider.promo_arrow-prev",
@@ -56,8 +49,16 @@ const myswiper2 = new Swiper(".promo-slider", {
 
   lazy: true,
 
-
   scrollbar: {
-    el: '.swiper-scrollbar',
+    el: ".swiper-scrollbar",
   },
 });
+
+// $(document).ready(function () {
+//   $(".input-checkbox").click(function () {
+//     $(this).parent().toggleClass("on");
+//   });
+// });
+
+
+
