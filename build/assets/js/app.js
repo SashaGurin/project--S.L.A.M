af73c4e997fa9b92cd81eb6a9f9bd38ab0ae9b07
SashaGurin <top-on3@mai.ru>
2022-02-23 15:28:20 +0300

    + adaptiveh

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -29,13 +29,20 @@ document
     swiper.autoplay.start();
   });
 
-
 // слайдер promo......
 const myswiper2 = new Swiper(".promo-slider", {
   loop: true,
   slidesPerView: 4,
   slidesPerGroup: 10,
   spaceBetween: 20,
+  // breakpointsInverse: true,
+  // dynamicMainBullets: 5,
+  //   breakpoints: {
+  // 768: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 20,
+  //      },
+  //  }
 
   pagination: {
     el: ".swiper-pagination",
@@ -47,20 +54,51 @@ const myswiper2 = new Swiper(".promo-slider", {
     nextEl: ".swiper-button-next.promo--slider.promo_arrow-next",
     prevEl: " .swiper-button-prev.promo--slider.promo_arrow-prev",
   },
+  breakpoints: {
+    // when window width is <= 575px
+    319: {
+      slidesPerView: 1,
+      spaceBetween: 40
+    },
+
+    840: {
+      slidesPerView: 1,
+      spaceBetween: 40
+    },
+    // when window width is <= 768px
+    864: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
+    // when window width is <= 992px
+    1150: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    1400: {
+      slidesPerView: 4,
+      spaceBetween: 20
+    }
+    },
   preloadImages: true,
-
   lazy: true,
-
   scrollbar: {
     el: ".swiper-scrollbar",
   },
 });
 
-// $(document).ready(function () {
-//   $(".chbx").click(function () {
-//     $(this).parent().toggleClass("on");
-//   });
-// });
 
 
-
+$(document).ready(function () {
+  $(".burger").on("click", function () {
+    $(".nav-list").toggleClass("active");
+  });
+});
